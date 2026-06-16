dbbc91a75834235bb3886db3842e1ef7667ece3f
Farrah <f.hidayatun@students.amikom.ac.id>
2026-06-16 20:59:55 +0700

    Add files via upload

---
+++ Login_Kelompok2.cpp
@@ -0,0 +1,248 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Pengguna 
+{
+    string username;
+    string password;
+    string role; 
+};
+
+void clearScreen()
+{
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+
+void kembaliKeMenu()
+{
+    cout << "\nTekan ENTER untuk kembali ke menu utama . . . ";
+    cin.ignore();
+    cin.get();
+}
+
+int main ()
+{
+    Pengguna databaseAkun[10];
+    int jumlahAkun = 0;
+    
+    while (true)
+    {
+        string roleUserAktif = ""; 
+        bool loginSukses = false;
+        
+        while (!loginSukses)
+        {
+            clearScreen();
+            cout << "==========================================" << endl;
+            cout << "                SISTEM GUDANG              " << endl;
+            cout << "==========================================" << endl;
+            cout << "1. Login ke Sistem" << endl;
+            cout << "2. Daftar Akun Baru" << endl;
+            cout << "3. Keluar dari Aplikasi" << endl;
+            cout << "___________________________________________" << endl;
+            cout << "Pilih opsi (1-3): ";
+            int opsiAwal;
+            cin >> opsiAwal;
+            
+            if (opsiAwal == 1) 
+            {
+                clearScreen(); 
+                if (jumlahAkun == 0)
+                {
+                    cout << "[!] PEMBERITAHUAN [!]" << endl;
+                    cout << "Belum ada akun yang terdaftar di sistem." << endl;
+                    cout << "Silahkan kembali dan pilih menu 'Daftar Akun Baru' terlebih dahulu." << endl;
+                    
+                    cout << "\nTekan ENTER untuk kembali. . .";
+                    cin.ignore();
+                    cin.get();
+                    continue; 
+                }
+                
+                string inputUser, inputPass;
+                cout << "--- HALAMAN LOGIN ---" << endl;
+                cout << "Username : "; cin >> inputUser; 
+                cout << "Password : "; cin >> inputPass; 
+                
+                for (int i = 0; i < jumlahAkun; i++)
+                {
+                    if (databaseAkun[i].username == inputUser && databaseAkun[i].password == inputPass)
+                    {
+                        loginSukses = true;
+                        roleUserAktif = databaseAkun[i].role;
+                        break;
+                    }
+                }
+                
+                if (loginSukses)
+                {
+                    cout << "\n[?] Login Berhasil! Anda masuk sebagai " << roleUserAktif << "." << endl;
+                    cout << "Tekan ENTER untuk masuk ke Menu Utama...";
+                    cin.ignore();
+                    cin.get();
+                }
+                else
+                {
+                    cout << "\n[X] Username atau Password salah!" << endl;
+                    cout << "Tekan ENTER untuk mencoba lagi...";
+                    cin.ignore();
+                    cin.get();
+                }
+            }
+            else if (opsiAwal == 2)
+            {
+                clearScreen();
+                if (jumlahAkun >= 10)
+                {
+                    cout << "[X] Memori database penuh! Tidak bisa menambah akun lagi." << endl;
+                    kembaliKeMenu();
+                    continue; 
+                }
+                
+                string newUser, newPass;
+                int pilihanRole;
+                
+                cout << "--- PENDAFTARAN AKUN BARU ---" << endl;
+                cout << "Masukkan Username Baru : ";
+                cin >> newUser; 
+                cout << "Masukkan Password Baru : ";
+                cin >> newPass;
+                cout << "Pilih Hak Akses (Role): \n1. ADMIN\n2. STAFF GUDANG\nPilihan (1-2): "; 
+                cin >> pilihanRole;
+                
+                string newRole = (pilihanRole == 1) ? "ADMIN" : "STAFF";
+                
+                databaseAkun[jumlahAkun].username = newUser;
+                databaseAkun[jumlahAkun].password = newPass;
+                databaseAkun[jumlahAkun].role = newRole;
+                jumlahAkun++;
+                
+                cout << "\n[?] Akun berhasil didaftarkan!" << endl;
+                cout << "Silahkan gunakan akun tersebut untuk login." << endl;
+                cout << "Tekan ENTER untuk kembali...";
+                cin.ignore();
+                cin.get();      
+            }
+            else if (opsiAwal == 3)
+            {
+                clearScreen();
+                cout << "Terima kasih telah menggunakan sistem." << endl;
+                return 0;   
+            }   
+        } 
+        
+        bool tetapDiMenuUtama = true;
+        while (tetapDiMenuUtama)
+        {
+            clearScreen(); 
+            cout << "===================================================" << endl;
+            cout << "               MENU UTAMA - " << roleUserAktif << endl;
+            cout << "===================================================" << endl;
+            
+            if (roleUserAktif == "ADMIN") 
+            {
+                cout << "1. Kelola Stok Barang & Vendor (CRUD)" << endl;
+                cout << "2. Cari Barang / Kode SKU" << endl;
+                cout << "3. Log Masuk-Keluar (Real-time)" << endl;
+                cout << "4. Cetak Barcode & Laporan" << endl;
+                cout << "5. Kembali ke Halaman Login (Logout)" << endl;
+                cout << "6. Keluar dari Aplikasi" << endl;
+            } 
+            else if (roleUserAktif == "STAFF") 
+            {
+                cout << "1. Cari Barang / Kode SKU" << endl;
+                cout << "2. Log Masuk-Keluar (Real-time)" << endl;
+                cout << "3. Kembali ke Halaman Login (Logout)" << endl;
+                cout << "4. Keluar dari Aplikasi" << endl;
+            }
+            cout << "___________________________________________________" << endl;
+            cout << "Pilihan menu: "; 
+            int pilihanMenu;
+            cin >> pilihanMenu;
+            
+            if (roleUserAktif == "ADMIN") 
+            {
+                switch (pilihanMenu)
+                {
+                    case 1:
+                        clearScreen();
+                        cout << "--- [MENU 1] KELOLA STOK BARANG & VENDOR ---" << endl;
+                        cout << "[?] Aksi berhasil dimuat oleh Admin." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 2:
+                        clearScreen();
+                        cout << "--- [MENU 2] CARI NAMA BARANG / SKU ---" << endl;
+                        cout << "[?] Menampilkan Data Pencarian Barang..." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 3:
+                        clearScreen();
+                        cout << "--- [MENU 3] LOG TRANSAKSI REAL-TIME ---" << endl;
+                        cout << "[?] Menampilkan Log Masuk-Keluar Barang Secara Real-Time..." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 4:
+                        clearScreen();
+                        cout << "--- [MENU 4] PRINT BARCODE & LAPORAN ---" << endl;
+                        cout << "[?] Dokumen cetak berhasil di-generate oleh Admin." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 5:
+                        tetapDiMenuUtama = false; 
+                        cout << "\nMelakukan Logout..." << endl;
+                        cout << "Tekan ENTER untuk kembali ke halaman Login...";
+                        cin.ignore(); cin.get();
+                        break;
+                    case 6:
+                        clearScreen();
+                        cout << "Terima kasih telah menggunakan sistem." << endl;
+                        return 0;
+                    default:
+                        cout << "\nPilihan tidak valid!" << endl;
+                        kembaliKeMenu();
+                        break;  
+                }
+            } 
+            else if (roleUserAktif == "STAFF") 
+            {
+                switch (pilihanMenu)
+                {
+                    case 1:
+                        clearScreen();
+                        cout << "--- [MENU 1] CARI NAMA BARANG / SKU ---" << endl;
+                        cout << "[?] Menampilkan Data Pencarian Barang..." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 2:
+                        clearScreen();
+                        cout << "--- [MENU 2] LOG TRANSAKSI REAL-TIME ---" << endl;
+                        cout << "[?] Menampilkan Log Masuk-Keluar Barang Secara Real-Time..." << endl;
+                        kembaliKeMenu();
+                        break;
+                    case 3:
+                        tetapDiMenuUtama = false; 
+                        cout << "\nMelakukan Logout..." << endl;
+                        cout << "Tekan ENTER untuk kembali ke halaman Login...";
+                        cin.ignore(); cin.get();
+                        break;
+                    case 4: 
+                        clearScreen();
+                        cout << "Terima kasih telah menggunakan sistem." << endl;
+                        return 0;
+                    default:
+                        cout << "\nPilihan tidak valid!" << endl;
+                        kembaliKeMenu();
+                        break;  
+                }
+            }
+        } 
+    } 
+    return 0;
+}
